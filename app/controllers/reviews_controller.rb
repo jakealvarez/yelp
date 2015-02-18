3bb869dcfb56f67cb71db5440682a268a1679dd5
Jake Alvarez <jakealvarez81@hotmail.com>
2015-02-18 18:46:05 +0000

    no reviews for deleted restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,14 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.reviews.create(review_params)
+    redirect_to restaurants_path
+  end
+
+  def review_params
+    params.require(:review).permit(:thoughts, :rating)
+  end
 
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -14,4 +14,20 @@ feature 'reviewing' do
     expect(page).to have_content('so so')
   end
 
+
+scenario 'where restaurant is deleted, reviews associated with the restaurant are also deleted' do
+
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    visit '/restaurants'
+    click_link 'Delete KFC'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).not_to have_content('Review KFC')
+    expect(page).not_to have_content('Delete KFC')
+  end
+
 end
